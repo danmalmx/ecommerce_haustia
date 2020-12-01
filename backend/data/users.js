b3c7002946bd5e34e446e9c4964d2e8ad7a0133d
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-01 20:31:27 +0100

    Add placeholder users

---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+import brcypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: brcypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'Mack Daddy',
+        email: 'mack@daddy.com',
+        password: brcypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Miss Piggy',
+        email: 'miss@piggy.com',
+        password: brcypt.hashSync('123456', 10),
+    },
+
+]
