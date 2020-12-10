16fbbc1da1265aaa70052da949d3cc8286468105
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-10 22:07:48 +0100

    Add reducer and action for product details

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import Product from '../models/productModel.js'
 
 // description: Fetch all products
-// route :      GET /api/producst
+// route :      GET /api/products
 // access:      Public
 router.get('/', asyncHandler (async (req, res) => {
     const products = await Product.find({});
@@ -13,7 +13,7 @@ router.get('/', asyncHandler (async (req, res) => {
 
 
 // description: Fetch a single product
-// route :      GET /api/products/:id
+// route :      GET /api/product/:id
 // access:      Public
 router.get('/:id', asyncHandler ( async (req, res) => {
     const product = await Product.findById(req.params.id);
