de9cde02f07c0e9cf3a4b06e0629cc6741866935
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-16 19:44:40 +0100

    Add custom auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,9 +8,9 @@ import generateToken from '../utilities/generateToken.js'
 
 const authUser = asyncHandler(async(req, res) => {
     const { email, password} = req.body;
-
+    
     const user = await User.findOne({ email });
-
+    
     if(user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
@@ -19,11 +19,33 @@ const authUser = asyncHandler(async(req, res) => {
             isAdmin: user.isAdmin,
             token: generateToken(user._id)
         })
-
+        
     } else {
         res.status(401);
         throw new Error('Invalid email or password')
     }
 });
 
-export { authUser }
+// description: Gwt user profile
+// route :      GET /api/users/profile
+// access:      Public
+
+const getUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name, 
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+
+    } else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+    
+});
+
+export { authUser, getUserProfile }
