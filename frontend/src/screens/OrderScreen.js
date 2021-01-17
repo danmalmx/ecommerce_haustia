bcb6b1a6084f8e98cd5e58280c100988a95f19cc
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-17 16:01:17 +0100

    Add front end funcioanlity to mark ordee as delivered by admin

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { PayPalButton }  from 'react-paypal-button-v2';
-import { Col, Row, ListGroup, Image, Card } from 'react-bootstrap';
+import { Col, Row, ListGroup, Image, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getOrderDetails, payOrder } from "../actions/orderActions";
-import { ORDER_PAY_RESET } from "../constants/orderConstants";
+import { getOrderDetails, payOrder, deliverOrder } from "../actions/orderActions";
+import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from "../constants/orderConstants";
 
-const OrderScreen = ({ match }) => {
+const OrderScreen = ({ match, history }) => {
     const orderId = match.params.id;
 
     let [sdkReady, setSdkReady] = useState(false)
@@ -19,8 +19,14 @@ const OrderScreen = ({ match }) => {
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, loading, error } = orderDetails;
 
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin;
+
     const orderPay = useSelector(state => state.orderPay)
     const { loading: loadingPay, success: successPay } = orderPay;
+
+    const orderDeliver = useSelector(state => state.orderDeliver)
+    const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
 
     if (!loading) {
         order.itemPrice = order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0);
@@ -28,6 +34,9 @@ const OrderScreen = ({ match }) => {
 
 
     useEffect(() => {
+        if (!userInfo) {
+            history.push('/login');
+        }
         const addPayPalScript = async () => {
             const { data: clientId } = await axios.get('/api/config/paypal');
             const script = document.createElement('script');
@@ -40,8 +49,9 @@ const OrderScreen = ({ match }) => {
             document.body.appendChild(script);
         }
 
-        if (!order || successPay) {
+        if (!order || successPay || successDeliver) {
             dispatch({ type: ORDER_PAY_RESET})
+            dispatch({ type: ORDER_DELIVER_RESET})
             dispatch(getOrderDetails(orderId));
         } else if (!order.isPaid) {
             if (!window.paypal) {
@@ -51,13 +61,16 @@ const OrderScreen = ({ match }) => {
             }
         }
 
-    }, [dispatch, orderId, successPay, order]);
+    }, [dispatch, orderId, successPay, successDeliver, order]);
 
     const sucessPaymentHandler = (paymentResult) => {
         console.log(paymentResult)
         dispatch(payOrder(orderId, paymentResult));
     }
 
+    const deliverHandler = () => {
+        dispatch(deliverOrder(order));
+    }
     return loading ? <Loader /> : error ? <Message variant="danger">{{error}}</Message> :
         <>
             <h1>Order {order._id}</h1>
@@ -160,12 +173,19 @@ const OrderScreen = ({ match }) => {
                                         onSuccess={sucessPaymentHandler} />}
                             </ListGroup.Item>
                         )}
+                        {loadingDeliver && <Loader />}
+                        {userInfo && userInfo.isAdmin === 'true' && order.isPaid & !order.isDelivered && (
+                            <ListGroup.Item>
+                                <Button type="button" className="btn btn-block" onClick={deliverHandler}>
+                                    Mark As Delivered
+                                </Button>
+                            </ListGroup.Item>
+                        )}
                         </ListGroup>
                     </Card>
                 </Col>
             </Row>
     </>
-
 }
 
 export default OrderScreen
