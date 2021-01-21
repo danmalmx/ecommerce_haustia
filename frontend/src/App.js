d32b6df4544b447a8bd4d34002b4f7c0789517e8
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-21 20:20:23 +0100

    Add pagination functionality to both home screen and for admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,11 +35,14 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListScreen}/>
           <Route path='/admin/users/:id/edit' component={UserEditScreen}/>
           <Route path='/products/:id' component={ProductScreen}/>
-          <Route path='/admin/productlist' component={ProductListScreen}/>
+          <Route path='/admin/productlist' component={ProductListScreen} exact />
+          <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact/>
           <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
           <Route path='/admin/orderlist' component={OrderListScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
-          <Route path='/search/:keyword' component={HomeScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact/>
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact/>
           <Route path='/' component={HomeScreen} exact />
       </Container>
     </main>
